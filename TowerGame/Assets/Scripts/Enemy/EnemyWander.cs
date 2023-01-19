0b1834ed8606cb77a23faf6ed5ec96491bbc8a9e
Alina-Alen <alina.alen.2003@gmail.com>
2023-01-18 22:29:21 -0800

    Added enemy wander

--- TowerGame/Assets/Scripts/Enemy/EnemyWander.cs
+++ TowerGame/Assets/Scripts/Enemy/EnemyWander.cs
@@ -15,9 +15,21 @@ public class EnemyWander : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsFacingRight())
+        {
+            //move right
+            rigidbody2D.velocity = new Vector2(speed, 0f);
+        }
+        else
+        {
+            //move left
+            rigidbody2D.velocity = new Vector2(-speed, 0f);
+        }
     }
-
+    private bool IsFacingRight()
+    {
+        return transform.localScale.x > 0.00001f;
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         transform.localScale = new Vector2(-(Mathf.Sign(rigidbody2D.velocity.x)), transform.localScale.y);
